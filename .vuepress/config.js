929b54ed5c8740cd5bcd79c0c938554ec803ab5a
Alex Krasnij <krasaler@gmail.com>
2019-07-27 09:50:17 +0300

    Changed logo

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,9 @@
 module.exports = {
   title: 'Rapin JS',
   description: 'Framework for creating REST API',
+  gtm: 'GTM-P6RXWS6',
   themeConfig: {
     repo: 'rapinjs/rapin',
-  }
+  },
+  plugins: ['vuepress-plugin-google-tag-manager'] 
 }
